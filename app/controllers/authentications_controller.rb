54cb41635b476c68f4f9ada9ec9f692719f8b181
kbackowski <kbackowski@gmail.com>
2012-11-18 14:27:03 +0100

    Refactored authentications create action

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,9 +1,7 @@
 class AuthenticationsController < ApplicationController
   def create
     omniauth = request.env["omniauth.auth"]
-    user = User.where(:email => omniauth.info.email).first || User.new
-    user.email = omniauth.info.email
-    user.facebook_uid = omniauth.uid
+    user = User.where(:email => omniauth.info.email, :facebook_uid => omniauth.uid).first_or_initialize
     if user.save
       sign_in :user, user
       redirect_to request.env["omniauth.origin"] || root_url
